41c818c67c586cd9077de87b00333a08f21fb705
G0834 <G0834@20G0834>
2023-11-30 09:54:56 +0900

    まーじできた

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+	}
+	static void kadaiA() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("現在" + i + "回目のループです。" );
+		}
+	}
+}
